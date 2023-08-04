53c938e7d31bc45906e6517dceb34e92abe24329
lamboforcats <55919965+lamboforcats@users.noreply.github.com>
2023-08-04 14:01:56 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 # server.py
 from flask import Flask, request, send_file, render_template
-import main.py
+from werkzeug.utils import secure_filename
+import os
+import main
 
 app = Flask(__name__)
+
+# Make sure to create 'uploads' directory in the same path where your server.py file is
+UPLOAD_FOLDER = 'uploads/'
 
 @app.route('/', methods=['GET'])
 def home():
@@ -12,7 +17,10 @@ def home():
 def process_pdf():
     if request.method == 'POST':
         file = request.files['file']
-        main.py(file)  # run your script
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
+        file.save(file_path)
+        main.process_pdf(file_path)  # run your script
         return send_file('attributes.xlsx', as_attachment=True)
     return render_template('index.html')
 
