5627b8ee019ee8c20d707ea2b6aea88720584296
lamboforcats <55919965+lamboforcats@users.noreply.github.com>
2023-08-04 13:43:05 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,20 @@
 # server.py
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, render_template
 import main
 
 app = Flask(__name__)
 
-@app.route('/process_pdf', methods=['POST'])
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/process_pdf', methods=['GET', 'POST'])
 def process_pdf():
-    file = request.files['file']
-    main(file)  # run your script
-    return send_file('attributes.xlsx', as_attachment=True)
+    if request.method == 'POST':
+        file = request.files['file']
+        main(file)  # run your script
+        return send_file('attributes.xlsx', as_attachment=True)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
